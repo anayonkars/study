011fb91f51a1e76d96b790c4c4c7905941db862f
anayonkars <anayonkar.shivalkar@gmail.com>
2015-12-29 15:55:54 +0530

    BitCoin NonceFinder

---
+++ CodeDemos/DemoUtils/src/main/java/code/demoutils/BitCoin/NonceFinder.java
@@ -0,0 +1,12 @@
+package code.demoutils.BitCoin;
+
+import java.util.concurrent.Callable;
+
+class NonceFinder implements Callable<Long> {
+
+	public Long call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CodeDemos/DemoUtils/src/main/java/code/demoutils/BitCoin/SHAMain.java
@@ -0,0 +1,10 @@
+package code.demoutils.BitCoin;
+
+public class SHAMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

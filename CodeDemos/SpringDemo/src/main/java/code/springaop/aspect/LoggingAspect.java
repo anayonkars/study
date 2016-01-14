a3a1d1ea398178d414a79b6bff97ac773a452363
anayonkars <anayonkar.shivalkar@gmail.com>
2016-01-14 13:32:06 +0530

    First PointCut

--- CodeDemos/SpringDemo/src/main/java/code/springaop/aspect/LoggingAspect.java
+++ CodeDemos/SpringDemo/src/main/java/code/springaop/aspect/LoggingAspect.java
@@ -2,12 +2,26 @@ package code.springaop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String getName())")
+	//@Before("execution(public String code.springaop.model.Circle.getName())")
+	//@Before("execution(public * get*())")
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("Advice run, get method called.");
 	}
+	
+	//when loggingAdvice and anotherAdvice have exactly same aspect criteria
+	//then anotherAdvice will run first because it comes first alphabetically
+	//@Before("execution(* get*())")
+	@Before("allGetters()")
+	public void anotherAdvice() {
+		System.out.println("Anoter advice run");
+	}
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}
 }

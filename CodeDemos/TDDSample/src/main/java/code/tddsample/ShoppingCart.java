1446595fd9aa106fa2936e2491f67090d828c52e
anayonkars <anayonkar.shivalkar@gmail.com>
2016-02-10 22:09:08 +0530

    TDD: Step 5 Test fix

--- CodeDemos/TDDSample/src/main/java/code/tddsample/ShoppingCart.java
+++ CodeDemos/TDDSample/src/main/java/code/tddsample/ShoppingCart.java
@@ -1,11 +1,19 @@
 package code.tddsample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anayonkar on 10/2/16.
  */
 public class ShoppingCart {
+    final List<Product> productList = new ArrayList<>();
     public int getProductCount() {
-        return 0;
+        return productList.size();
     }
-    public void addProduct(int quantity, Product product) {}
+    public void addProduct(int quantity, Product product) {
+        for(int i = 0 ; i < quantity ; i++) {
+            productList.add(product);
+        }
+    }
 }
